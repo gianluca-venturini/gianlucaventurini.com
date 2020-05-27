e89663e64a2560aa088cbef8bc58b5cb792c9113
Gianluca Venturini <gianluca@stellarite.io>
2020-05-27 16:58:52 -0700

    Home

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { Home } from './components/pages/Home';
 
 export class App extends React.Component<{}> {
 
     render() {
-        return <div>Hello</div>;
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" component={Home} />
+                </Switch>
+            </BrowserRouter>
+        );
     }
 
 }
