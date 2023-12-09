f7a7cf51b5e3cf7a90d2f2b8db5b17dee0cbd4bf
Gianluca Venturini <gianluca.91@gmail.com>
2023-12-09 15:42:17 -0800

    Configure eslint and prettier to work with Nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.css';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div>Home</div>
-    </main>
-  )
+    return (
+        <main className={styles.main}>
+            <div>Home</div>
+        </main>
+    );
 }
