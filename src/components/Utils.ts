00d6ee0339fdd5462e78d5ba9d9de5699384df8d
Gianluca Venturini <gianluca.91@gmail.com>
2023-12-19 10:25:23 -0800

    Format date

---
+++ src/components/Utils.ts
@@ -0,0 +1,13 @@
+export function formatDate(date: string) {
+    if (!date) return '';
+    const d = new Date(date);
+    if (isNaN(d.getTime())) return '';
+    let month = '' + (d.getMonth() + 1);
+    let day = '' + d.getDate();
+    const year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
