9786a3dae1e1fbacbaa068050d5cad7b945513c3
Gianluca Venturini <gianluca.91@gmail.com>
2023-12-19 16:08:33 -0800

    Improve bookshelf

--- src/pages/bookshelf/index.tsx
+++ src/pages/bookshelf/index.tsx
@@ -13,7 +13,22 @@ export default function Bookshelf(
         data: props.data,
     });
 
-    return <div>{JSON.stringify(data)}</div>;
+    return (
+        <div className="flex flex-col">
+            <h1 className="text-5xl font-bold mb-4">My Bookshelf</h1>
+            {data.bookshelf.book?.map((book) => (
+                <div key={book?.title} className="flex flex-row gap-2">
+                    <span className="text-sm whitespace-nowrap">
+                        {book?.title}
+                    </span>
+                    <span className="text-sm">&mdash;</span>
+                    <span className="text-sm font-thin whitespace-nowrap">
+                        {book?.author}
+                    </span>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export const getStaticProps = async () => {
