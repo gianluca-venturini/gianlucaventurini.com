25cac8332e36897e5126f4ab60074e884838dca5
Gianluca Venturini <gianluca.91@gmail.com>
2023-12-24 16:26:33 -0800

    Add post unsoundness-with-typescript-getters and code snippets

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,5 +1,7 @@
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
 
+import { Code } from './Code';
+
 export const Markdown = (props: Parameters<typeof TinaMarkdown>[0]) => {
     return (
         <span className="markdown">
@@ -9,6 +11,11 @@ export const Markdown = (props: Parameters<typeof TinaMarkdown>[0]) => {
 };
 
 const components: Record<string, (props: object) => JSX.Element> = {
+    // Override default markdown components
+    code_block: (props: any) =>
+        props.lang &&
+        props.value && <Code language={props.lang}>{props.value}</Code>,
+    // Custom components
     Visualization: (props: { title?: string }) => <h1>{props.title}</h1>,
     Video: (props: { src?: string }) =>
         props.src ? (
