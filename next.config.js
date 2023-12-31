ed91a99bbfb3b5e26019c16198891a0d74b0c6ab
Gianluca Venturini <gianluca.91@gmail.com>
2023-12-31 08:33:41 -0800

    Redirect /blog/:path* -> /posts/:path*

--- next.config.js
+++ next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
             },
         ]
     },
+    async redirects() {
+        return [
+          {
+            source: '/blog/:path*',
+            destination: '/posts/:path*',
+            permanent: true
+          },
+        ]
+    },    
 };
 
 module.exports = nextConfig
