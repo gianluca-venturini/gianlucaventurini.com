9915cf2e96f6c0e67dafab419e11c05f1ff2ce7f
Gianluca Venturini <gianluca.91@gmail.com>
2024-08-10 20:50:59 -0700

    Feed rss

---
+++ src/app/rss.xml/route.ts
@@ -0,0 +1,37 @@
+import client from '../../../tina/__generated__/client';
+import { BASE_URL } from '../../constants';
+
+export const GET = async () => {
+    const { data } = await client.queries.postConnection({
+        sort: 'date',
+        last: 10,
+    });
+
+    const posts = data.postConnection.edges
+        ?.map((e) => e?.node)
+        .filter((n) => !!n);
+
+    const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+    <channel>
+    <title>Gianluca Venturini RSS Feed</title>
+    <link>${BASE_URL}</link>
+    <description>Gianluca Venturini's personal blog</description>
+    ${posts
+        ?.map(
+            (p) => `        <item>
+            <title>${p?.title}</title>
+            <link>${BASE_URL}/posts/${p?._sys.filename}</link>
+            <description>${p?.snippet}</description>
+        </item>`
+        )
+        .join('\n')}
+    </channel>
+</rss>`;
+
+    return new Response(rssFeed, {
+        headers: {
+            'Content-Type': 'text/xml',
+        },
+    });
+};
